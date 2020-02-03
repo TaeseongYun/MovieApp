79c1bdd7992be9eb1539360df32a68f8c087105e
TaeseongYun <yts0646@gmail.com>
2020-02-04 00:21:38 +0900

    feat: README flow-control

--- README.markdown
+++ README.markdown
@@ -5,6 +5,9 @@
 
 - subscribeOn? observeOn? -> 아직까직은 자세히 어떻게 동작하는지 잘 모름. 후에 공부하고나서 변경예정. 대략적으로 subscribeOn은 메인 Observable 함수가 돌아가는 스레드를 지정 observeOn은 부가적인 데이터가 돌아가는 스레드 지정으로 알고있음.
 
+- `흐름제어함수`는 데이터를 발행하는 속도와 데이터를 받아서 처리하는 속도가 차이가 발생할 때 사용하는 함수. (주로 UI 클릭이벤트에 사용된다는글을 본적 있음) 이번에는 throttleLast를 사용하였음. 매개변수로 50L TimeUnit.Millisecond를 넣었다. 즉 0.5초 동안 마지막에 들어온 데이터를 발행한다는 뜻.
+
 ## LiveData
 
 - behaviorSubject를 이용하면서 livedata는 백그라운드 스레드에서 동작하지 못하는다는 점을 알게되었다. Room과는 정반대 Room은 메인 스레드에서 동작하지 못함.
+
--- app/src/main/java/tsthec/tsstudy/movieapplicationmvvmstudy/ui/movie/MovieMainActivity.kt
+++ app/src/main/java/tsthec/tsstudy/movieapplicationmvvmstudy/ui/movie/MovieMainActivity.kt
@@ -9,8 +9,6 @@ import android.view.Menu
 import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import io.reactivex.Observable
-import io.reactivex.functions.BiFunction
-import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
 import kotlinx.android.synthetic.main.activity_main.*
 import tsthec.tsstudy.movieapplicationmvvmstudy.R
@@ -22,11 +20,37 @@ import tsthec.tsstudy.movieapplicationmvvmstudy.util.loadFragment
 import tsthec.tsstudy.movieapplicationmvvmstudy.util.log.LogUtil
 import tsthec.tsstudy.movieapplicationmvvmstudy.util.plusAssign
 import java.lang.Thread.sleep
-import java.util.*
 import java.util.concurrent.TimeUnit
+
 
 @Suppress("CAST_NEVER_SUCCEEDS")
 class MovieMainActivity : BaseActivity() {
+
+    init {
+        val source1: Observable<Any> =
+            Observable.interval(100L, TimeUnit.MILLISECONDS)
+                .map { i -> i * 100 }
+                .map { i -> LogUtil.d("Source1 -> $i") }
+                .take(10)
+
+        val source2: Observable<Any> =
+            Observable.interval(300L, TimeUnit.MILLISECONDS)
+                .map { i -> i * 300 }
+                .map { i -> LogUtil.d("Source2 -> $i") }
+                .take(3)
+
+        val source3: Observable<Any> =
+            Observable.interval(2000L, TimeUnit.MILLISECONDS)
+                .map { i -> i * 2000 }
+                .map { i -> LogUtil.d("Source3 -> $i") }
+                .take(2)
+
+        disposable += Observable.concat(source1, source2, source3)
+            .throttleLast(100L, TimeUnit.MILLISECONDS)
+            .subscribe { it }
+
+        sleep(6000)
+    }
 
     //백버튼을 눌렀을 때 pair first 값과 second 두 개의 값을 emit 했을 때 2000(2초) 보다 적으면 finish()
     private val backKeyPressSubject = BehaviorSubject.create<Pair<Double, Double>>()
