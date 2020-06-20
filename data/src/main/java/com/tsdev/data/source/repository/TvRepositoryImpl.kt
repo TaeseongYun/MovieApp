45780a3deacf9a8da1315f0a923850167407eea0
TaeseongYun <yts0646@gmail.com>
2020-06-21 01:42:32 +0900

    Modify : TvRepositoryImpl

--- data/src/main/java/com/tsdev/data/source/repository/TvRepositoryImpl.kt
+++ data/src/main/java/com/tsdev/data/source/repository/TvRepositoryImpl.kt
@@ -1,3 +1,9 @@
 package com.tsdev.data.source.repository
 
-internal class TvRepositoryImpl
+import com.tsdev.data.local.MovieLocalSourceData
+import com.tsdev.data.remote.MovieRemoteSourceData
+
+internal class TvRepositoryImpl(private val remoteSourceData: MovieRemoteSourceData,
+                                private val localSourceData: MovieLocalSourceData) : TvRepository {
+
+}
